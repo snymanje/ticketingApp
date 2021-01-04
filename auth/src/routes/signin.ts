900f17aa8889c2d1aa63ac2dd760ca398a106124
jeansn <jeansn@tfg.co.za>
2021-01-04 15:32:06 +0200

    added current-user and signout routes

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,50 @@
-import express from 'express';
+import { Password } from './../services/password';
+import jwt from "jsonwebtoken";
+import { BadRequestError } from './../errors/BadRequestError';
+import express, { Response, Request } from "express";
+import { body } from "express-validator";
+import { User } from "../models/user";
+import { validateRequest } from "./../middlewares/validate-request";
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Hello')
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if(!existingUser) {
+        throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsmatch = await Password.compare(existingUser.password, password);
+
+    if(!passwordsmatch) {
+        throw new BadRequestError('Invalid credentials');
+    }
+
+    // generate jwt
+    const userJwt = jwt.sign(
+        {
+          id: existingUser.id,
+          email: existingUser.email,
+        },
+        process.env.JWT_KEY as string
+      );
+  
+      req.session = {
+        jwt: userJwt,
+      };
+  
+      res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,51 +1,52 @@
-import { BadRequestError } from './../errors/BadRequestError';
-import express, { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
-import { body, validationResult } from 'express-validator';
-import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from "./../errors/BadRequestError";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { body } from "express-validator";
+import { User } from "../models/user";
+import { validateRequest } from "./../middlewares/validate-request";
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-    body('email')
-        .isEmail()
-        .withMessage('Email must be valid'),
-    body('password')
-        .trim()
-        .isLength({ min: 4, max: 20 })
-        .withMessage('Password must be between 4 and 20 characters')
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
-
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
     const { email, password } = req.body;
-    
-    const existingUser = await User.findOne({ email })
 
-    if(existingUser) {
-        throw new BadRequestError('Email in use.')
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email in use.");
     }
-    
-    const user = new User;
+
+    const user = new User();
     user.email = email;
     user.password = password;
 
     await user.save();
 
-    // generate jwt 
-    const userJwt = jwt.sign({
+    // generate jwt
+    const userJwt = jwt.sign(
+      {
         id: user.id,
-        email: user.email
-    }, process.env.JWT_KEY as string);
+        email: user.email,
+      },
+      process.env.JWT_KEY as string
+    );
 
     req.session = {
-        jwt: userJwt
-    }
+      jwt: userJwt,
+    };
 
-    res.status(201).send(user)
-});
+    res.status(201).send(user);
+  }
+);
 
 export { router as signupRouter };
